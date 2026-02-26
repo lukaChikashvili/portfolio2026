ded8f63368f090d7a7c3a0c260891aed5513d0ea
luka chikashvili <lukachikashvili2020@gmail.com>
2026-02-26 20:04:03 +0400

    add button for experience

--- portfolio/src/components/HomePage.jsx
+++ portfolio/src/components/HomePage.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="absolute inset-0 opacity-10 pointer-events-none" 
+    <div className="flex h-screen items-center justify-center ">
+      <div className="absolute inset-0 opacity-10 pointer-events-none " 
            style={{ backgroundImage: 'repeating-linear-gradient(to right, #000 0px, #000 1px, transparent 1px, transparent 200px)' }}>
       </div>
-    <div className="flex flex-col items-center text-center gap-4 -mt-36">
+    <div className="flex flex-col items-center text-center gap-4 ">
       <h1 className="title">
         Luka Chikashvili
       </h1>
@@ -14,7 +14,22 @@ const HomePage = () => {
       <h2 className='text-5xl slogan '>
         Developer as an <span className='underline underline-offset-6 '>Innie</span>, Polyglot as an <span>Outie</span>
       </h2>
+
+    <div className='mt-12'>
+      <button className="button ">
+  <div className="button-outer">
+    <div className="button-inner">
+      <span>Start Experience</span>
     </div>
+  </div>
+</button>
+
+</div>
+
+    </div>
+
+   
+   
   </div>
   )
 }
